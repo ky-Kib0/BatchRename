a4c71017e30aaf16a4bc47361199745f2ffacfb4
ky-Kib0 <66004846+ky-Kib0@users.noreply.github.com>
2022-03-28 03:05:37 +0800

    Delete 中间产物 directory

--- 中间产物/CreateSrc.bat
+++
@@ -1,3 +0,0 @@
-dir /a-d /b *.png>src.txt
-echo success！
-pause
--- 中间产物/rename.bat
+++
@@ -1,8 +0,0 @@
-setlocal enabledelayedexpansion
-set /a a=0
-@for  /f %%s in (src.txt) do (
-set /a a+=1
-rename %%s 000!a!.png
-)
-echo success！
-pause
